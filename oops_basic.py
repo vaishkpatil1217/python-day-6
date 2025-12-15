9789266cf0e078568609d725091a649e555b88d3
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-15 15:33:26 +0530

    basic oops

---
+++ oops_basic.py
@@ -0,0 +1,10 @@
+class vehical:
+    def __init__(self,max_speed,mileage):
+        
+        self.max_speed=max_speed
+        self.mileage=mileage
+        
+modelx =vehical(240,18)
+
+print(modelx.max_speed,modelx.mileage)
+        
