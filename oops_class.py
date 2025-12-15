c4535dea3a88625ef4e9d6df403962986124fe2a
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-15 15:48:56 +0530

    Class Inheritance

---
+++ oops_class.py
@@ -0,0 +1,18 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+    
+class bus(Vehicle):
+    
+    def seating_capacity(self,capacity=50):
+        return super().seating_capacity(capacity=50)
+    
+school_bus=bus("volvo bus",180,12)
+
+print(school_bus.seating_capacity())
+
